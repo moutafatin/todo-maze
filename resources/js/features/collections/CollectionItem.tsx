5682d49457ea7205db0b45de8730c0517c21291d
Oussama Moutafatin <moutafatin3@gmail.com>
2024-05-08 08:15:17 +0100

    preserve sidebar state when navigating between collections

--- resources/js/features/collections/CollectionItem.tsx
+++ resources/js/features/collections/CollectionItem.tsx
@@ -14,7 +14,7 @@ export function CollectionItem({collection}: CollectionItemProps) {
     return <li
         className='flex items-center gap-x-2'>
         <Link href={route('todos.index', {folder: collection.folder_id, collection: collection.id})}
-              className='w-full'>
+              className='w-full' preserveState>
             <Button variant='ghost' className='gap-x-2 w-full justify-start focus-visible:ring-1'>
                 <ListIcon/>
                 <span>{collection.name}</span>
